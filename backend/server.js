0e8bc15f5a70771223c8c1d32f58bc3925dbdce4
Panita Donmuang <panipani.d903@gmail.com>
2025-09-25 16:55:31 +0700

    update vercel.json

--- backend/server.js
+++ backend/server.js
@@ -10,38 +10,36 @@ import mongoose from "mongoose"
 dotenv.config()
 const app = express()
 
-// const allowedOrigins = [
-//   "https://full-stack-blog-2025.vercel.app",
-//   "http://localhost:5173",
-//   "https://full-stack-blog-2025-git-working-panitas-projects.vercel.app",
-//   "https://full-stack-blog-2025-u77o.vercel.app" // backend URL ที่ deploy
-// ];
+const allowedOrigins = [
+  "https://full-stack-blog-2025.vercel.app",
+  "http://localhost:5173",
+  "https://full-stack-blog-2025-git-working-panitas-projects.vercel.app",
+  "https://full-stack-blog-2025-u77o.vercel.app" // backend URL ที่ deploy
+];
 
 // เปิด CORS ก่อน
-app.use(cors({
-  origin: "http://localhost:5173", // port ที่ frontend ใช้
-  credentials: true,
-}));
-
-// mongoose.connect(process.env.DB_LOCATION) // จริงๆ มีใน lib/connectDB แล้ว
-//     .then(()=> console.log("MongoDB conneted!"))
-//     .catch(err => console.error("Mongo error: ", err))
 // app.use(cors({
 //   origin: "http://localhost:5173", // port ที่ frontend ใช้
 //   credentials: true,
 // }));
-/*
+
 app.use(cors({
-  origin: function(origin, callback) {
-     console.log("Request Origin:", origin); 
-    if (!origin || allowedOrigins.includes(origin) || origin.startsWith("https://full-stack-blog-2025")) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));*/
+  origin: ["https://full-stack-blog-2025.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
+
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//      console.log("Request Origin:", origin); 
+//     if (!origin || allowedOrigins.includes(origin) || origin.startsWith("https://full-stack-blog-2025")) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
 
 app.use(express.json())
     
@@ -63,25 +61,4 @@ app.listen(3000,() => {
     connectDB()
     console.log("Server is running!")
 })
-
-// allow cross-origin requests
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", 
-//     "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-// app.get("/test", (req, res) => {
-//     res.status(200).send("It works!")
-// } )
-
-// mongoose.connect(process.env.DB_LOCATION) //มีใน lib/connectDB แล้ว
-//     .then(()=> console.log("MongoDB conneted!"))
-//     .catch(err => console.error("Mongo error: ", err))
-// app.use(cors({
-//   origin: "http://localhost:5173", // หรือ port ที่ frontend ใช้
-//   credentials: true,
-// }));
-
 
